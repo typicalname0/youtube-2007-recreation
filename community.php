ee2889a51de974d8750c370ff259946d246bd8df
pesopeso9868 <24258996+pesopeso9868@users.noreply.github.com>
2021-01-04 20:39:14 -0500

    Initial commit

---
+++ community.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<?php include 'global.php';?>
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/global.css">
+	<link rel="stylesheet" type="text/css" href="./css/community.css">
+</head>
+<body>
+	<?php include "header.php"; ?>
+</body>
+<?php $mysqli->close();?>
---
+++ subscribe.php
@@ -0,0 +1,28 @@
+<?php include 'global.php';?>
+<?php
+include("header.php");
+
+session_start();
+if(!isset($_SESSION['profileuser3'])) {
+    die("login to subscribe...");
+} 
+else{
+    if(!isset($_GET['id'])){
+        die("no");
+    } 
+    else{
+        $a = (int)$_GET['id'];
+        $u = $_GET['u'];
+        if((bool)$u === true){
+            $mysqli->query("UPDATE users SET subscribers = subscribers-1 WHERE id = '".$a."' LIMIT 1");
+            $_SESSION['subscribedto'.$a] = false;
+        }
+        else{
+            $mysqli->query("UPDATE users SET subscribers = subscribers+1 WHERE id = '".$a."'");
+            $_SESSION['subscribedto'.$a] = true;
+        }
+        header("Location: ".$_SERVER['HTTP_REFERER']."");
+    }
+}
+?>
+<?php $mysqli->close();?>
